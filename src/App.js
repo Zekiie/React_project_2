1d73333590fdaeab36978d1ad37079ed0301a6c8
Zekiie Irikh <zekie.irikh@gmail.com>
2019-07-24 08:12:47 +0300

    WIP: Header with Toolbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 // import classes from './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-// import Backdrop from ',/src/components/Ui/Backdrop/Backdrop';
 class App extends Component {
   render() {
     return (
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,6 +82,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: false});
   };
 
+  purchaseContionueHandler = () => {
+    // this.setState({purchasing: false});
+  };
+
   render() {
 
     const disabledInfo = {
@@ -94,7 +98,11 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients}/>
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinue={this.purchaseContionueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
