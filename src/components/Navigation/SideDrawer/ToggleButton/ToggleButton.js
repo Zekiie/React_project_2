f99555bb894314e876db0a17a04e257f47414fc1
Zekiie Irikh <zekie.irikh@gmail.com>
2020-05-10 20:22:15 +0300

    Added SideDrawer, ToogleButton

---
+++ src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ToggleButton = (props) => {
+    return (
+        <div onClick={clicked}>Menu</div>
+    )
+};
+
+export default ToggleButton
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
+import ToggleButton from "../SideDrawer/ToggleButton/ToggleButton";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <ToggleButton />
     <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
